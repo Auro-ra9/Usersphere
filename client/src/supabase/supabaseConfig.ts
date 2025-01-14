3e9b1b474c500d25048be0d5c0adfd7a9c2bf862
Auro-ra9 <ashnaahammed9@gmail.com>
2025-01-14 19:45:29 +0530

    set up supabase storage config

---
+++ client/src/supabase/supabaseConfig.ts
@@ -0,0 +1,8 @@
+// Import the Supabase client
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
